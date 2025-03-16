0975707d0a8ab6db43e1ed59320b9a03dbbd2b66
gitMiko0 <dustinbengo@gmail.com>
2025-03-16 12:13:10 -0600

    fixed loading state issue

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -5,12 +5,11 @@ import React from "react";
  * This current implementation is a bit hardcoded, should be improved to adjust to screen height.
  */
 const Loading = ({ count = 11 }) => {
-    console.log("Rendering Loading component");
     const placeholderDivs = [];
     for (let i = 0; i < count; i++) {
       placeholderDivs.push(
         <div key={i} className="mt-0 space-y-4 flex justify-center">
-          <div className="mt-4 h-10 backdrop-blur bg-opacity-30 items-center bg-white rounded-xl"></div>
+          <div className="w-11/12 mt-4 h-10 backdrop-blur bg-opacity-30 items-center bg-white rounded-xl"></div>
         </div>
       );
     }
